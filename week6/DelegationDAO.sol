4617bfcb0bc0ba7b551b9495fe34eaa2c369b685
Henry (Wei) <hyd628@gmail.com>
2022-04-08 21:48:58 +0800

    Create DelegationDAO.sol

---
+++ week6/DelegationDAO.sol
@@ -0,0 +1,166 @@
+// SPDX-License-Identifier: GPL-3.0-only
+// This is a PoC to use the staking precompile wrapper as a Solidity developer.
+pragma solidity >=0.8.0;
+
+import "./StakingInterface.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract DelegationDAO is AccessControl {
+    using SafeMath for uint256;
+    
+    // Role definition for contract members (approved by admin)
+    bytes32 public constant MEMBER = keccak256("MEMBER");
+
+    enum daoState{ COLLECTING, STAKING, REVOKING, REVOKED }
+
+    daoState currentState; 
+
+    // Member stakes (doesnt include rewards, represents member shares)
+    mapping(address => uint256) public memberStakes;
+    
+    // Total Staking Pool (doesnt include rewards, represents total shares)
+    uint256 public totalStake;
+
+    // The ParachainStaking wrapper at the known pre-compile address. This will be used to make
+    // all calls to the underlying staking solution
+    ParachainStaking public staking;
+    
+    // Minimum Nomination Amount
+    uint256 public constant MinNominatorStk = 5 ether;
+    
+    // Moonbem Staking Precompile address
+    address public constant stakingPrecompileAddress = 0x0000000000000000000000000000000000000800;
+
+    // The collator that this DAO is currently nominating
+    address public target;
+
+    // Initialize a new NominationDao dedicated to nominating the given collator target.
+    constructor(address _target, address admin) {
+        
+        //Sets the collator that this DAO nominating
+        target = _target;
+        
+        // Initializes Moonbeam's parachain staking precompile
+        staking = ParachainStaking(stakingPrecompileAddress);
+        
+        //Initializes Roles
+        _setupRole(DEFAULT_ADMIN_ROLE, admin);
+        _setupRole(MEMBER, admin);
+
+        //Initialize DAO state
+        currentState = daoState.COLLECTING;
+        
+    }
+
+    // Grant a user the role of admin
+    function grant_admin(address newAdmin)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        onlyRole(MEMBER)
+    {
+        grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
+        grantRole(MEMBER, newAdmin);
+    }
+
+    // Grant a user membership
+    function grant_member(address newMember)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        grantRole(MEMBER, newMember);
+    }
+
+    // Revoke a user membership
+    function remove_member(address payable exMember)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        revokeRole(MEMBER, exMember);
+    }
+
+    // Add stake (and increase pool share)
+    function add_stake() external payable onlyRole(MEMBER) {
+        if (currentState == daoState.STAKING ) {
+            // Sanity check
+            if(!staking.is_delegator(address(this))){
+                 revert("The DAO is in an inconsistent state.");
+            }
+            memberStakes[msg.sender] = memberStakes[msg.sender].add(msg.value);
+            totalStake = totalStake.add(msg.value);
+            staking.delegator_bond_more(target, msg.value);
+        }
+        else if  (currentState == daoState.COLLECTING ){
+            memberStakes[msg.sender] = memberStakes[msg.sender].add(msg.value);
+            totalStake = totalStake.add(msg.value);
+            if(totalStake < MinNominatorStk){
+                return;
+            } else {
+                //initialiate the delegation and change the state          
+                staking.delegate(target, address(this).balance, staking.candidate_delegation_count(target), staking.delegator_delegation_count(address(this)));
+                currentState = daoState.STAKING;
+            }
+        }
+        else {
+            revert("The DAO is not accepting new stakes in the current state.");
+        }
+    }
+
+    // Function for a user to withdraw their stake
+    function withdraw(address payable account) public onlyRole(MEMBER) {
+        if (currentState == daoState.COLLECTING || currentState == daoState.STAKING) {
+            revert("The DAO is not in the correct state to withdraw");
+        }
+
+        if (currentState == daoState.REVOKING) {
+            bool result = execute_revoke();
+            if (!result) {
+                return;
+            }
+        }
+        //Calculate the withdraw amount including staking rewards
+        uint amount = address(this)
+            .balance
+            .mul(memberStakes[msg.sender])
+            .div(totalStake);
+
+        Address.sendValue(account, amount);
+        totalStake = totalStake.sub(memberStakes[msg.sender]);
+        memberStakes[msg.sender] = 0;
+    }
+
+    // Schedule revoke, admin only
+    function schedule_revoke() public onlyRole(DEFAULT_ADMIN_ROLE){
+        if (currentState != daoState.STAKING) {
+            revert("The DAO is not in the correct state to schedule a revoke.");
+        }
+        staking.schedule_revoke_delegation(target);
+        currentState = daoState.REVOKING;
+    }
+    
+    // Try to execute the revoke, returns true if it succeeds, false if it doesn't
+    function execute_revoke() public onlyRole(MEMBER) returns(bool) {
+        if (currentState != daoState.REVOKING) {
+            revert("The DAO is not in the correct state to execute a revoke.");
+        }
+        try staking.execute_delegation_request(address(this), target) {
+            currentState = daoState.REVOKED;
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    // Check how much free balance the DAO currently has. It should be the staking rewards if the DAO state is anything other than REVOKED. 
+    function check_free_balance() public view onlyRole(MEMBER) returns(uint256){
+        return address(this).balance;
+    }
+    
+    // Change the collator target, admin only
+    function change_target(address newCollator) public onlyRole(DEFAULT_ADMIN_ROLE){
+        target = newCollator;
+    }
+
+
+}
